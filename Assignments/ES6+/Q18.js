b4c5df1bb65af367913aae04f176bb78f00f1738
unknown <ganagallajoshitha@gmail.com>
2025-11-24 20:12:05 +0530

    Q18.js is added

--- Assignments/ES6+/Q18.js
+++ Assignments/ES6+/Q18.js
@@ -1,4 +1,12 @@
-?
+//a
+if (true) {
+  let x = 10;
+  var y = 20;
+}
+console.log(y);
+console.log(x);
+//var y is function/global-scoped, so it's accessible outside the if block and logs 20. let x is block-scoped, so it’s not accessible outside the block, and logging it would error.
+//b
 const profile = {
   user: {
     details: {
@@ -9,3 +17,10 @@ const profile = {
 console.log(profile?.user?.details?.email);
 console.log(profile?.user?.details?.phone);
 
+//c
+const user = {
+  name: "Jane Doe"
+};
+const userAge = user.profile?.age;
+console.log(`User age: ${userAge}`);
+console.log("The program did not crash.");
